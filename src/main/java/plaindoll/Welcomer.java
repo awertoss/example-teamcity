5207ae8f289e7db4903230c4f49f9689b211e1a0
awertoss <109097833+awertoss@users.noreply.github.com>
2023-04-10 14:14:04 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -14,7 +14,7 @@ public class Welcomer{
 		return "something in the way";
 	}
 
-public String sayHunter(){
+        public String sayHunter(){
                 return "Return of the hunter";
         }
 }
